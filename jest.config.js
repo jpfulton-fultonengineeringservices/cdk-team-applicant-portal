45b8afcd9de5ae050e2279655b21803cfdfb5c98
J. Patrick Fulton <patrick@fultonengineeringservices.com>
2026-02-19 13:59:17 -0600

    chore: move isolatedModules from ts-jest config to tsconfig.json
    Signed-off-by: J. Patrick Fulton <patrick@fultonengineeringservices.com>

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
   roots: ['<rootDir>/test'],
   testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { isolatedModules: true }],
+    '^.+\\.tsx?$': 'ts-jest',
   },
 };
